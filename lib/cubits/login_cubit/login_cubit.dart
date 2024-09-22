06dcd7c4bb7267cc0289541b86dc7d213cba2eed
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-22 16:55:33 +0300

    create login method cubit

--- lib/cubits/login_cubit/login_cubit.dart
+++ lib/cubits/login_cubit/login_cubit.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
+
+  Future<void> loginUser(
+      {required String email, required String password}) async {
+    emit(LoginLoading());
+    try {
+      // ignore: unused_local_variable
+      UserCredential user = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      emit(LoginSuccess());
+    } catch (e) {
+      emit(LoginFailure());
+    }
+  }
 }
