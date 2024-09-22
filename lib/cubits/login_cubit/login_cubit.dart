ef165980fe4ae61cf1c74002477733e2819bd1c6
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-22 16:49:01 +0300

    create login cubit

---
+++ lib/cubits/login_cubit/login_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+}
---
+++ lib/cubits/login_cubit/login_state.dart
@@ -0,0 +1,11 @@
+part of 'login_cubit.dart';
+
+sealed class LoginState {}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginSuccess extends LoginState {}
+
+final class LoginFailure extends LoginState {}
+
+final class LoginLoading extends LoginState {}
