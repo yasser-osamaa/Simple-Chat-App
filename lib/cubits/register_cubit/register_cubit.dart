96c69a386c5c799237d8530d08c49429dc3891c1
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-22 17:35:11 +0300

    create cubit and method of registeration

---
+++ lib/cubits/register_cubit/register_cubit.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  RegisterCubit() : super(RegisterInitial());
+
+  Future<void> registerUser(
+      {required String email, required String password}) async {
+    emit(RegisterLoading());
+    try {
+      UserCredential user =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(RegisterSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        emit(RegisterFailuer(errMsg: 'Weak Password'));
+      } else if (e.code == 'email-already-in-use') {
+        emit(RegisterFailuer(errMsg: 'Email Already Exist'));
+      }
+    } catch (e) {
+      emit(RegisterFailuer(errMsg: 'something wrong'));
+    }
+  }
+}
